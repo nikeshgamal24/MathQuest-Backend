0d1e494dbdd9754f61189071bf4ced9784c4229b
Nikesh Gamal <nikesh.gamal24@gmail.com>
2025-03-28 22:18:41 +0545

    update allowed origin file

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,4 @@
 //creating whiteList in order to ensure your server is accessed by limited domain
-const allowedOrigins = ["http://localhost:5173", "https://localhost:3500", "http://localhost:8080"];
+const allowedOrigins = ["http://localhost:5173", "https://localhost:3500", "http://localhost:8080","https://math-quest-fun.vercel.app"];
 
 export default allowedOrigins;
