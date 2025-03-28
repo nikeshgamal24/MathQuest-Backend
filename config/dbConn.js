5b18008e6a8f97a58f1a244cf8b63ceaaf0dd8d4
Nikesh Gamal <nikesh.gamal24@gmail.com>
2025-03-28 22:14:48 +0545

    hosted db connection details

--- config/dbConn.js
+++ config/dbConn.js
@@ -5,15 +5,15 @@ const { Pool } = pkg;
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DBPORT,
+  connectionString: process.env.DB_URL,
 });
 
 pool.on("connect", () => {
   console.log("Connection pool established with Database");
+});
+
+pool.on("error", (err) => {
+  console.error("PostgreSQL Pool Error:", err);
 });
 
 export default pool;
